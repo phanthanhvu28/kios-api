6f40ad6b0bcc045e1f85a1e6dbc1ef112903ca8e
Phan Thanh Vu <dieter.vu@vela.com.vn>
2024-08-28 11:56:45 +0700

    feat: api table by store

---
+++ ApplicationCore/Specifications/Table/TableByStoreCodeSpec.cs
@@ -0,0 +1,11 @@
+﻿using ApplicationCore.Contracts.SpecificationBase;
+
+namespace ApplicationCore.Specifications.Table;
+public class TableByStoreCodeSpec : SpecificationBase<Entities.Tables>
+{
+    public TableByStoreCodeSpec(string storeCode)
+    {
+        ApplyFilter(entity => entity.StoreCode == storeCode);
+        ApplyFilter(entity => entity.IsDelete == false);
+    }
+}
---
+++ ApplicationCore/UseCases/Table/Queries/GetTableByStore.cs
@@ -0,0 +1,49 @@
+﻿using ApplicationCore.Contracts.RepositoryBase;
+using ApplicationCore.DTOs.Table;
+using ApplicationCore.Specifications.Table;
+using ApplicationCore.ValueObjects;
+using Mapster;
+using Mediator;
+using VELA.WebCoreBase.Core.Mediators;
+using VELA.WebCoreBase.Core.Models;
+using VELA.WebCoreBase.Core.PipelineBehaviors;
+
+namespace ApplicationCore.UseCases.Table.Queries;
+public class GetTableByStore : IItemQuery<List<TableBaseDto>>
+{
+    public string StoreCode { get; set; }
+    public sealed class Handler : IQueryHandler<GetTableByStore, ResultModel<List<TableBaseDto>>>
+    {
+        private readonly IStoreRepository _storeRepository;
+        private readonly ITableRepository _tableRepository;
+        private readonly IAreaRepository _areaRepository;
+        private readonly ITypeSaleRepository _typeSaleRepository;
+        private readonly ITypeBidaRepository _typeBidaRepository;
+        private readonly IdentityUserObject? _identityUser;
+
+        public Handler(
+            IStoreRepository storeRepository,
+            ITableRepository tableRepository,
+            IAreaRepository areaRepository,
+            ITypeSaleRepository typeSaleRepository,
+            ITypeBidaRepository typeBidaRepository,
+            IAppContextAccessor appContextAccessor)
+        {
+            _storeRepository = storeRepository;
+            _areaRepository = areaRepository;
+            _tableRepository = tableRepository;
+            _typeSaleRepository = typeSaleRepository;
+            _typeBidaRepository = typeBidaRepository;
+            _identityUser = appContextAccessor.IdentityUser?.Adapt<IdentityUserObject>();
+        }
+        public async ValueTask<ResultModel<List<TableBaseDto>>> Handle(GetTableByStore query, CancellationToken cancellationToken)
+        {
+            List<TableBaseDto> result = new();
+            TableByStoreCodeSpec tableSpec = new(query.StoreCode);
+            List<Entities.Tables> table = await _tableRepository.FindAsync(tableSpec);
+            result = table.Adapt<List<TableBaseDto>>();
+
+            return ResultModel<List<TableBaseDto>>.Create(result);
+        }
+    }
+}
